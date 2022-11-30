6ca7bb5526884c0019c78bcacd825ca91a9ce787
softwarerbfl <32202018@dankook.ac.kr>
2022-12-01 00:58:13 +0900

    sort 기본 문제 풀이

--- Sort/2587.py
+++ Sort/2587.py
@@ -1,7 +1,9 @@
 import sys
 l=[]
+
 for i in range(5):
     l.append(int(sys.stdin.readline()))
 l.sort()
+
 print(int(sum(l)/5))
 print(l[2])
--- Sort/2750.py
+++ Sort/2750.py
@@ -2,6 +2,7 @@ import sys
 
 N=int(sys.stdin.readline())
 l=[]
+
 for i in range(N):
     l.append(int(sys.stdin.readline()))
 for i in range(N):
