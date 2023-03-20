43219d4a5a43cd977e8c29eed1fcbff8bf70a49b
softwarerbfl <32202018@dankook.ac.kr>
2023-03-20 13:34:47 +0900

    미로 탐색

--- graph/2178.py
+++ graph/2178.py
@@ -1,19 +1,36 @@
 #bfs를 사용한 미로탐색
 import sys
-sys.setrecursionlimit(10000) #깊이 제한 10000으로 풀어줌
+from collections import deque
+dx=[0,0,-1,1]
+dy=[1,-1,0,0]
+
 n, m=map(int, sys.stdin.readline().split(" "))
 graph=[]
 for i in range(n):
     graph.append(list(map(int, input())))
 
-def bfs(a,b):
-    global n,m
-    x=a-1
-    y=b-1
-    if x<=0 or x>n or y<=0 or y>m:
-        return 0
-    #이동할 수 없는 칸이면 False 반환
-    if graph[x][y]==0:
-        return 0
+move=deque()
+move.append([0,0])
+count=1
+graph[0][0]=0
+result=1
+while True:
+    result+=1
+    tmp_count=count
+    count=0 # 해당 칸수만큼 이동했을 때 노드 개수들
+    for _ in range(tmp_count):
+        y,x=move.popleft()
 
-bfs(n,m) #n,m까지 가는 가장 빠른 이동 횟수 return
+        for i in range(4):
+            Y = y + dy[i]
+            X = x + dx[i]
+            # 영역 밖에 있거나 길이 없는 경우 스택에 넣지 않음
+            if Y < 0 or X < 0 or X > m - 1 or Y > n - 1 or graph[Y][X] == 0:
+                continue
+
+            if Y == n - 1 and X == m - 1:
+                print(result)
+                exit()
+            move.append([Y, X])
+            graph[Y][X] = 0  # 방문처리
+            count += 1  # 해당 칸수에서 노드 개수 증가
