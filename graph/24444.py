abfa9249a9368665c7ec60356786c490bedd939e
softwarerbfl <32202018@dankook.ac.kr>
2023-03-27 20:56:48 +0900

    알고리즘 수업 - 너비 우선 탐색1

---
+++ graph/24444.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+n, m, r = map(int, sys.stdin.readline().split())
+graph={i:[] for i in range(1,n+1)}
+visit=[0]*(n+1)
+q=deque()
+for _ in range(m):
+    u,v =map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+visit[r]=1 # 시작 지점 방문 처리
+q.append(r)
+count=1 # 방문 순서
+while q:
+    tmp=graph[q.popleft()] # 방문한 큐와 연결되어 있는 정점들
+    tmp.sort()
+    for t in tmp:
+        # 아직 방문안한 노드인 경우
+        if visit[t]==0:
+            count+=1
+            visit[t]=count # 방문처리
+            q.append(t)
+for i in range(n):
+    print(visit[i+1])
