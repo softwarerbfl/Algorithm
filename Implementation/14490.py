f269faa3468849fecbfb599e8bc4367e79e7da02
softwarerbfl <32202018@dankook.ac.kr>
2023-05-02 16:49:01 +0900

    백대열

---
+++ Implementation/14490.py
@@ -0,0 +1,11 @@
+﻿import sys
+
+n,m = map(int, sys.stdin.readline().split(":"))
+x=min([min([n, m]),abs(n-m)])
+
+for i in range(x, 0,-1):
+    if n%i==0 and m%i==0:
+        n=n/i
+        m=m/i
+        break
+print(":".join(map(str,[int(n),int(m)])))
