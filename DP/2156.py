ff99667f14670248129c1aab143e3ce7b237b7b4
softwarerbfl <32202018@dankook.ac.kr>
2023-06-03 22:23:27 +0900

    포도주 시식

---
+++ DP/2156.py
@@ -0,0 +1,12 @@
+import sys
+n=int(sys.stdin.readline())
+w = [0]
+for i in range(n):
+    w.append(int(sys.stdin.readline()))
+dp = [0]
+dp.append(w[1])
+if n > 1:
+    dp.append(w[1] + w[2])
+for i in range(3, n + 1):
+    dp.append(max(dp[i - 1], dp[i - 3] + w[i - 1] + w[i], dp[i - 2] + w[i]))
+print(dp[n])
